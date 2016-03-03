d01e3adc4490ebb0187dd1ae4e90d9239f2c4f79
Gabo <gabezurita@gmail.com>
2016-03-03 13:07:56 -0300

    Complete 4.3

---
+++ week-4/define-method/my_solution.rb
@@ -0,0 +1,12 @@
+# Define an Empty Method
+
+# I worked on this challenge [by myself].
+
+
+# Your Solution Below
+
+def do_something(a, b, c)
+	puts a
+	puts b
+	puts c
+end
---
+++ week-4/favorite_number.rb
@@ -0,0 +1,4 @@
+puts 'Hello there, and what\'s your favorite number?'
+favorite_number = gets.chomp
+better_number = favorite_number.to_i + 1
+puts 'Oh? my favorite is ' + better_number.to_s
