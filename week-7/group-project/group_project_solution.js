5c50a4592e07b7a9f967616a57601a6495d62677
Gabo <gabezurita@gmail.com>
2016-03-26 21:45:48 -0500

    Input work corresponding to person 1.

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,27 @@
+
+// Person 1: Test to User Stories
+
+// As a user, I'm in need of three functions that determine the sum, mean, and median of the numbers from two lists. 
+// One list will be of odd length and one will be of even length.
+
+// 	1. Sum Function:
+// 		a. Inputting a list of numbers of odd length into this function will return a sum of all values from this list.
+// 		b. Inputting a list of numbers of even length into this same function will also return a sum of all values from this list.
+// 	2. Mean Function:
+// 		a. Inputting a list of numbers of odd length will return the mean/average of the numbers in this list.
+// 		b. Inputting a list of numbers of even length will return the mean/average of the numbers in this list.
+// 	3. Median Function:
+// 		a. Inputting a list of numbers of odd length will return the median value of the numbers in this list.
+// 		b. Inputting a list of numbers of even length will return the median value of the numbers in this list.
+
+// Person 2: Pseudocode
+
+
+
+// Person 3: Solution
+
+
+
+// Person 1:
+
+
